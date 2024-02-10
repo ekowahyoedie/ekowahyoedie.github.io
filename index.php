1ce572602f9e893815e3f724ac4dfbc0fae88f2f
ekowahyoedie <89058392+ekowahyoedie@users.noreply.github.com>
2024-02-11 01:07:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,29 +2,12 @@
 
 
 
-  echo geturl($_GET['url'], '#EXTVLCOPT:http-referrer=https://www.visionplus.id/'); //untuk referernya
+  echo file_get_contents("https://bali-airport.com/data-airline/arr/domestic/"); 
 
-    function geturl($url, $referer) { 
-
+        $headers[] = 'Access-Control-Allow-Origin: https://bali-airport.com/en/arrival-terminal/index'; 
+        $headers[] = 'Access-Control-Allow-Credentials: true'; 
         $headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg,text/html,application/xhtml+xml'; 
         $headers[] = 'Connection: Keep-Alive'; 
-        $headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8'; 
         $useragent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)'; 
         
-        $process = curl_init($url); 
-        curl_setopt($process, CURLOPT_HTTPHEADER, $headers); 
-        curl_setopt($process, CURLOPT_HEADER, 0); 
-       
-        curl_setopt($process, CURLOPT_USERAGENT, $useragent);
-        curl_setopt($process, CURLOPT_REFERER, $referer);
-        curl_setopt($process, CURLOPT_TIMEOUT, 0); 
-        curl_setopt($process, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1); 
-        
-        $return = curl_exec($process); 
-        curl_close($process); 
-
-        return $return; 
-    } 
-
-?>
+?>        
